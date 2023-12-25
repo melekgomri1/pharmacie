0895224b0aabb0db4fd6f7e37687c9ad7d1e24eb
melek gomri <malekgomri881@hotmail.com>
2023-12-25 12:47:04 +0100

    terminer job

---
+++ skeleton/src/Form/JobType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Categoryjob;
+use App\Entity\Job;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class JobType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('prenom')
+            ->add('email')
+            ->add('numtelephone')
+            ->add('competance')
+            ->add('categoryjob', EntityType::class, [
+                'class' => Categoryjob::class,
+'choice_label' => 'name',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Job::class,
+        ]);
+    }
+}
